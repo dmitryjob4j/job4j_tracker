fa9632393ccc79e02361f9c0ac62157a7eb3026a
dmitryjob4j <haoos@inbox.ru>
2020-03-19 21:58:11 +0300

    1.1. Создание объекта[#242913]

---
+++ src/main/java/ru/job4j/tracker/Cat.java
@@ -0,0 +1,12 @@
+package ru.job4j.tracker;
+
+/**
+ * 1.1. Создание объекта[#242913]. пример
+ */
+
+public class Cat {
+    public static void main(String[] args) {
+        Cat peppy = new Cat();
+        Cat sparky = new Cat();
+    }
+}
---
+++ src/main/java/ru/job4j/tracker/Dog.java
@@ -0,0 +1,15 @@
+package ru.job4j.tracker;
+
+/**
+ * 1.1. Создание объекта[#242913]
+ *
+ * @author dstepanov haoos@inbox.ru
+ * Задание к теме создать класс Dog и создать три обьекта класса.
+ */
+public class Dog {
+    public static void main(String[] args) {
+        Dog polkan = new Dog();
+        Dog sharik = new Dog();
+        Dog zhychka = new Dog();
+    }
+}
