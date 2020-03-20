0391d72c1a76513e7fbebc545d3b212f52334913
dmitryjob4j <haoos@inbox.ru>
2020-03-20 21:09:25 +0300

    1.5. Поля объекта[#242914]

--- src/main/java/ru/job4j/tracker/oop/Cat.java
+++ src/main/java/ru/job4j/tracker/oop/Cat.java
@@ -1,20 +1,37 @@
 package ru.job4j.tracker.oop;
 
 /**
- * 1.4. Вызов метод с возвращаемым типом.[#242918] пример
+ * 1.5. Поля объекта[#242914]
  *
  * @since 20.03.2020
  */
 
 public class Cat {
-    public String sound() {
-        String voice = "may-may";
-        return voice;
+    private String food;
+    private String name;
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public void show() {
+        System.out.println(this.name + " " + this.food);
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
     }
 
     public static void main(String[] args) {
-        Cat peppy = new Cat();
-        String say = peppy.sound();
-        System.out.println("Peppy says " + say);
+        System.out.println("There are gav's food.");
+        Cat gav = new Cat();
+        gav.giveNick("Masel");
+        gav.eat("kotleta");
+        gav.show();
+        System.out.println("There are black's food.");
+        Cat black = new Cat();
+        black.giveNick("Popo");
+        black.eat("fish");
+        black.show();
     }
 }
