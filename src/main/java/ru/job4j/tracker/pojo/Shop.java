2a8cb39f6c6507e49f91ef0e4b2fbd9e13c48aa0
dmitryjob4j <haoos@inbox.ru>
2020-03-26 00:57:32 +0300

    3. Удаление моделей из массива.[#242934]

---
+++ src/main/java/ru/job4j/tracker/pojo/Shop.java
@@ -0,0 +1,56 @@
+package ru.job4j.tracker.pojo;
+
+public class Shop {
+    public Product[] delete(Product[] products, int index) {
+        for (int i = index; i < products.length - 1; i++) {
+            products[i] = products[i + 1];
+        }
+        products[products.length - 1] = null;
+        return products;
+    }
+
+    public static void main(String[] args) {
+        Product products[] = new Product[5];
+        products[0] = new Product("Milk", 10);
+        products[1] = new Product("Bread", 4);
+        products[2] = new Product("Egg", 19);
+        Shop dl = new Shop();
+        products = dl.delete(products, 2);
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+        /*
+        System.out.println();
+        System.out.println("Удаление значения из ячейки с индексом 1");
+        //Удаление значения из ячейки с индексом 1
+        products[1] = null;
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+        System.out.println();
+        System.out.println("Записываем  в ячейку с индексом 1 значение ячейки с " +
+                "индексом 2 и удаляем значение из ячейки с индексом 2");
+        //записываем в ячейку с индексом 1 значение ячейки с индексом 2.
+        products[1] = products[2];
+        //удоляем значение из ячейки с индексом 2
+        products[2] = null;
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }*/
+    }
+}
