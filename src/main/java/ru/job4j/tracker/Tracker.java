17479dcf81081f20cf83118523fcb5e2c4eb27c2
dmitryjob4j <haoos@inbox.ru>
2020-03-26 23:43:17 +0300

    5. Реализовать класс Tracker[#242937]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,20 +1,79 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * 5. Реализовать класс Tracker[#242937]
+ * задание.
+ *
+ * @version 1
+ * @since 26.03.2020
+ */
 public class Tracker {
+    /**
+     * Массив для хронения заявок на 100 элементов.
+     */
     private final Item[] items = new Item[100];
     private int ids = 1;
+    /**
+     * Указатель ячейки для новой заявки.
+     */
     private int size = 0;
 
+    /**
+     * 1 метод добавление заявок
+     *
+     * @param item новая заявка
+     */
     public Item add(Item item) {
         item.setId(generateId());
         items[size++] = item;
         return item;
     }
 
-    private String generateId() {
-        return String.valueOf(ids++);
+    /**
+     * 2 метод получение списка всех заявок без Null
+     *
+     * @return возврощаем заполненый массив без ячеек Null.
+     */
+    public Item[] findAll() {
+        Item[] nameWithoutNull = new Item[size];
+        int num = 0;
+        for (int i = 0; i < size; i++) {
+            Item name = items[i];
+            if (name != null) {
+                nameWithoutNull[num] = name;
+                num++;
+            }
+        }
+        return Arrays.copyOf(nameWithoutNull, num);
     }
 
+    /**
+     * 3 метод получение списка по имени
+     *
+     * @param key имя по которому ищем
+     * @return возвращаем новый массив имен
+     */
+    public Item[] findByName(String key) {
+        Item[] byName = new Item[size];
+        int num = 0;
+        for (int i = 0; i < size; i++) {
+            Item name = items[i];
+            if (name.getName().equals(key)) {
+                byName[num] = name;
+                num++;
+            }
+        }
+        return Arrays.copyOf(byName, num);
+    }
+
+    /**
+     * 4 Метод получение заявки по ID
+     *
+     * @param id заявки которую ищем.
+     */
     public Item findById(String id) {
         Item rsl = null;
         for (int i = 0; i < size; i++) {
@@ -25,5 +84,16 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    /**
+     * Метод гененрации уникального ключа для заявок
+     *
+     * @return Уникальный ключ.
+     **/
+    private String generateId() {
+        Random rm = new Random();
+        return String.valueOf(rm.nextLong() + System.currentTimeMillis());
+        //return String.valueOf(ids++);
     }
 }
--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -1,6 +1,7 @@
 package ru.job4j.tracker;
 
 import org.junit.Test;
+
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -12,5 +13,51 @@ public class TrackerTest {
         tracker.add(item);
         Item result = tracker.findById(item.getId());
         assertThat(result.getName(), is(item.getName()));
+    }
+
+    @Test
+    public void findAll() {
+        Tracker tracker = new Tracker();
+        Item item1 = new Item("test1");
+        Item item2 = new Item("test2");
+        Item item3 = new Item("test3");
+        Item item4 = new Item("test4");
+        tracker.add(item1);
+        tracker.add(item2);
+        tracker.add(item3);
+        tracker.add(item4);
+        Item[] expect = new Item[]{item1, item2, item3, item4};
+        Item[] result = tracker.findAll();
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void findByName(){
+        Tracker tracker = new Tracker();
+        Item item1 = new Item("test1");
+        Item item2 = new Item("test2");
+        Item item3 = new Item("test3");
+        Item item4 = new Item("test4");
+        tracker.add(item1);
+        tracker.add(item2);
+        tracker.add(item3);
+        tracker.add(item4);
+        Item[] expect = new Item[]{item2};
+        Item[] result = tracker.findByName("test2");
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void findByID(){
+        Tracker tracker = new Tracker();
+        Item item1 = new Item("test1");
+        Item item2 = new Item("test2");
+        Item item3 = new Item("test3");
+        Item item4 = new Item("test4");
+        tracker.add(item1);
+        tracker.add(item2);
+        tracker.add(item3);
+        tracker.add(item4);
+        Item expect = item3;
+        Item result = tracker.findById(item3.getId());
+        assertThat(result,is(expect));
     }
 }
