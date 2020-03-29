e6993a6748661916a1c6e3230d2ceaebe07cf9fa
dmitryjob4j <haoos@inbox.ru>
2020-03-30 01:50:49 +0300

    2. Scanner и чтение числа из консоли.[#242882]

---
+++ src/main/java/ru/job4j/tracker/io/Matches.java
@@ -0,0 +1,39 @@
+package ru.job4j.tracker.io;
+
+import java.util.Scanner;
+
+/**
+ * 2. Scanner и чтение числа из консоли.[#242882]
+ * задача "Сделать игру 11 спичек."
+ *
+ * @since 30.03.2020
+ */
+public class Matches {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int match = 11;
+        int gamer = 1;
+        int gamerNum = 11;
+        int select = 0;
+        System.out.println("+++Доброй пожаловать в игру 11 спичек.+++" + "\n" +
+                "+++Условия игры: Играют двое Господ.+++" + "\n" +
+                "+++Каждый Господин может брать от 1-й до 3-х спичек+++" + "\n" + "+++Начинаем+++");
+        while (match > 0) {
+            System.out.print("Господин №" + gamer + " берет спички -> ");
+            select = Integer.valueOf(input.nextLine());
+            if (select <= 3 && select > 0) {
+                match -= select;
+                if (match <= 0) {
+                    System.out.print("Господин №" + gamer + " выиграл игру!" + "\n" + "Игра окончена");
+                    break;
+                } else {
+                    gamerNum--;
+                    gamer = (gamerNum % 2 != 0) ? 1 : 2;
+                    System.out.println("Осталось " + match + " спичек");
+                }
+            } else {
+                System.out.println("Господин №" + gamer + " взял неверное количиство спичек!");
+            }
+        }
+    }
+}
