ebe6c08dbcd527cbcc5c1aa0f50c7b11e2926875
dmitryjob4j <haoos@inbox.ru>
2020-04-01 23:53:56 +0300

    5.1. Тестирование. Подготовка данных.[#242890]

--- src/test/java/ru/job4j/tracker/pojo/StartUITest.java
+++ src/test/java/ru/job4j/tracker/pojo/StartUITest.java
@@ -16,4 +16,25 @@ public class StartUITest {
         Item expected = new Item("Fix PC");
         assertThat(created.getName(),is(expected.getName()));
     }
+    @Test
+    public void whenReplaceItem(){
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {item.getId(), "replaced item"};
+        StartUI.replaceItem(new StubInput(answers), tracker);
+        Item replaced = tracker.findById(item.getId());
+        assertThat(replaced.getName(), is("replaced item"));
+    }
+    @Test
+    public void whenDeleteItem(){
+        Tracker tracker = new Tracker();
+        Item item = new Item("delete item");
+        String expected = item.getId();
+        tracker.add(item);
+        String[] answers = {item.getId()};
+        String deleteItem = item.getId();
+        StartUI.deleteItem(new StubInput(answers),tracker);
+        assertThat(tracker.findById(deleteItem),is(expected));
+    }
 }
