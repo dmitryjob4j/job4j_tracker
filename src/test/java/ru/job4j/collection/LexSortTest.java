ab2c87a586c2ee9be5f898caed2462ca39d50fb2
dmitryjob4j <haoos@inbox.ru>
2020-04-22 01:26:45 +0300

    4. Сортировка номера.[#242842]

---
+++ src/test/java/ru/job4j/collection/LexSortTest.java
@@ -0,0 +1,30 @@
+package ru.job4j.collection;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * 4. Сортировка номера.[#242842]
+ * тест.
+ */
+public class LexSortTest {
+    @Test
+    public void sortNum1and2and10() {
+        String[] input = {
+                "10. Task.",
+                "1. Task.",
+                "2. Task."
+        };
+        String[] out = {
+                "1. Task.",
+                "2. Task.",
+                "10. Task."
+        };
+        Arrays.sort(input, new LexSort());
+        assertThat(input, is(out));
+    }
+}
