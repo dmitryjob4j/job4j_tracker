5cfa987f448254249e2ff7a6ed191c16d999fc3d
dmitryjob4j <haoos@inbox.ru>
2020-04-22 22:52:48 +0300

    3. Компаратор для строк.[#242841]

--- src/main/java/ru/job4j/collection/StringCompare.java
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -4,31 +4,21 @@ import java.util.Comparator;
 
 /**
  * 3. Компаратор для строк.[#242841]
+ * @version 2
  */
 public class StringCompare implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
         int l = left.length();
         int r = right.length();
-        int index = 0;
-        int sizeRsl = Integer.compare(l, r);
-        int n = sizeRsl <= 0 ? l : r;
-        while (index < n) {
-            char chL = left.charAt(index);
-            char chR = right.charAt(index);
-            if (chL != chR) {
-                sizeRsl = Character.compare(chL, chR);
+        int size = Math.min(l, r);
+        int rsl = 0;
+        for (int i = 0; i < size; i++) {
+            if (left.charAt(i) != right.charAt(i)) {
+                rsl = Character.compare(left.charAt(i), right.charAt(i));
                 break;
-            } else if (index == n - 1 && l == r) {
-                sizeRsl = Character.compare(chL, chR);
-                break;
-            } else if ((index == n - 1) && (sizeRsl > 0 || sizeRsl < 0)) {
-                sizeRsl = Integer.compare(l, r);
-                break;
-            } else {
-                index++;
             }
         }
-        return sizeRsl;
+        return rsl == 0 ? Integer.compare(l, r) : rsl;
     }
 }
