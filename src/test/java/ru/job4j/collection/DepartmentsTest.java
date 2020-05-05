9c6e0924e1b1595291e9b468e298684aa5ef3857
dmitryjob4j <haoos@inbox.ru>
2020-05-05 10:52:41 +0300

    1. Collection API Улучшения[#242713]

--- src/test/java/ru/job4j/collection/DepartmentsTest.java
+++ src/test/java/ru/job4j/collection/DepartmentsTest.java
@@ -8,18 +8,24 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 /**
+ * 3. Jdk 1.9, 1.10 Нововведения.
+ * 1. Collection API Улучшения[#242713]
  * Отсортировать департаменты[#242838]
  * Тест на добовление недостоющих элементов и прямую сортировку.
  *
  * @author D.Stepanov
  * @version 1
- * @since 26.04.2020
+ * @since 05.05.2020
  */
 public class DepartmentsTest {
     @Test
     public void whenMissed() {
-        List<String> input = Arrays.asList("k1/ks1/kss2");
-        List<String> expect = Arrays.asList("k1", "k1/ks1", "k1/ks1/kss2");
+        List<String> input = List.of("k1/ks1/kss2");
+        List<String> expect = List.of(
+                "k1",
+                "k1/ks1",
+                "k1/ks1/kss2"
+        );
         List<String> result = Departments.fillGraps(input);
         Departments.sortAsc(result);
         assertThat(result, is(expect));
@@ -27,8 +33,17 @@ public class DepartmentsTest {
 
     @Test
     public void whenNonChange() {
-        List<String> input = Arrays.asList("k1", "k1/sk1", "k2/sk2/ssk3");
-        List<String> expect = Arrays.asList("k1", "k1/sk1", "k2", "k2/sk2", "k2/sk2/ssk3");
+        List<String> input = List.of("k1",
+                "k1/sk1",
+                "k2/sk2/ssk3"
+        );
+        List<String> expect = List.of(
+                "k1",
+                "k1/sk1",
+                "k2",
+                "k2/sk2",
+                "k2/sk2/ssk3"
+        );
         List<String> result = Departments.fillGraps(input);
         Departments.sortAsc(result);
         assertThat(result, is(expect));
